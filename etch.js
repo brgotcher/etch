716afeeb5f925faf9037dd6fff347573423b8305
Reed Gotcher <brgotcher@gmail.com>
2018-05-31 04:12:13 -0500

    add clear button and allow custom board size

--- etch.js
+++ etch.js
@@ -1,28 +1,42 @@
-const container = document.querySelector('#container');
 
-for (let i = 0; i < 16; i++) {
-	let row = document.createElement('div');
-	row.classList.add('row');
-	container.appendChild(row);
-	for (let j = 0; j < 16; j++) {
-		let cell = document.createElement('div');
-		cell.classList.add('cell');
-		row.appendChild(cell);
+let btn = document.createElement('button');
+btn.textContent = "Clear the board";
+let body = document.querySelector('body');
+const container = document.querySelector('#container');
+body.insertBefore(btn, container);
+let number = 16;
+let row;
+let cell;
+
+function buildGrid(num) {
+	let size = 1000 / num;
+	for (let i = 0; i < num; i++) {
+		row = document.createElement('div');
+		row.classList.add('row');
+		row.style.height = `${size}px`;
+		container.appendChild(row);
+		for (let j = 0; j < num; j++) {
+			cell = document.createElement('div');
+			cell.classList.add('cell');
+			cell.style.width = `${size}px`;
+			row.appendChild(cell);
+		}
 	}
+	let cellList = document.querySelectorAll('.cell')
+	cellList.forEach((block) => {
+		block.addEventListener('mouseover', (e) => {
+			block.classList.add('activated');
+		})
+	})
 }
 
+buildGrid(number);
 
-let cellList = document.querySelectorAll('.cell')
 
-cellList.forEach((block) => {
+
+btn.addEventListener('click', (e) => {
 	console.log("test");
-	block.addEventListener('mouseover', (e) => {
-		block.classList.add('activated');
-		console.log("test");
-	})
-	block.addEventListener('mouseout', (e) => {
-		setTimeout(function() {
-			block.classList.remove('activated');
-		}, 2500);
-	})
+	container.innerHTML = "";
+	number = prompt("What size grid would you like?");
+	buildGrid(number);
 })
